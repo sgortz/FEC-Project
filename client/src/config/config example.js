26946a6f7f674119a32b08713aa097680c01b1ae
Huijun Lu <lorwegy@gmail.com>
2022-03-30 15:05:59 -0400

    project setup completed

---
+++ client/src/config/config example.js
@@ -0,0 +1,4 @@
+var API_KEY = 'YOUR_API_KEY_HERE';
+var FEC_API_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/rfe/';
+
+export { API_KEY, FEC_API_URL };
---
+++ server/routes.js
@@ -0,0 +1,14 @@
+var controller = require('./controllers');
+var router = require('express').Router();
+
+//Connect controller methods to their corresponding routes
+// example:
+// router.get('/pageDetail', controller.pageDetail.get);
+
+
+
+
+
+
+module.exports = router;
+
