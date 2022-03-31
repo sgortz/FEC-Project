8c4cf3fd3965878e3703e4618830fbf1b932cb11
Katy Feng <97313452+katyfsy@users.noreply.github.com>
2022-03-31 12:54:33 -0400

    review model functions

---
+++ server/controllers/review.js
@@ -0,0 +1,30 @@
+const models = require('../models');
+
+
+module.exports = {
+  getAllreviews: function (req, res) {
+
+
+  },
+
+  getReviewMeta: function (req, res) {
+
+  },
+
+  writeReview: function (req, res) {
+
+  },
+
+  putHelpful: function (req, res) {
+
+  },
+
+  putReport: function (creq, res) {
+
+  }
+
+
+
+
+
+}
