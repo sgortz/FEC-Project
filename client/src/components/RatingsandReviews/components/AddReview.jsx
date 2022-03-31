1684db5dd51df22e342a3b3a6b9836ccc65b5295
Katy Feng <97313452+katyfsy@users.noreply.github.com>
2022-03-31 00:48:21 -0400

    create ratingsreviews components

---
+++ client/src/components/RatingsandReviews/components/AddReview.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function AddReview () {
+
+  return(
+    <div>
+      <h3>AddReview</h3>
+    </div>
+  )
+
+};
+
+export default AddReview;
