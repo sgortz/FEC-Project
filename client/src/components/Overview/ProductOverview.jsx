ea0e199f951f2ed3beba3afbcf460b4bfe1619c2
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-03-31 22:06:01 -0400

    Announcement moved to a component, applied grid to layout

--- client/src/components/Overview/ProductOverview.jsx
+++ client/src/components/Overview/ProductOverview.jsx
@@ -1,25 +1,51 @@
 import React from 'react';
 import NavBar from './components/NavBar.jsx';
+import Announcements from './components/Announcements.jsx';
 import ImageGallery from './components/ImageGallery.jsx';
 import StarReview from './components/StarReview.jsx';
 import ProductDetail from './components/ProductDetail.jsx';
 import StyleSelector from './components/StyleSelector.jsx';
 import AddToCart from './components/AddToCart.jsx';
 import ProductDescription from './components/ProductDescription.jsx';
+import stylesExample from '../../Data/overviewStylesExample.js';
+import './ProductOverview.css';
 
-const ProductOverview = () => {
-  return (
-    <div>
-      <NavBar />
-      <small>SITE-WIDE ANNOUNCEMENT MESSAGE! -- SALE / DISCOUNT offer -- NEW PRODUCT HIGHLIGHT</small>
-      <ImageGallery />
-      <StarReview />
-      <ProductDetail />
-      <StyleSelector />
-      <AddToCart />
-      <ProductDescription />
-    </div>
-  )
+console.log('styles! ', stylesExample)
+
+class ProductOverview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Announcements />
+        <div className="wrapper">
+          <div className="one">
+            <ImageGallery styles={stylesExample} />
+          </div>
+          <div className="two">
+            <StarReview />
+          </div>
+          <div className="three">
+            <ProductDetail />
+          </div>
+          <div className="four">
+            <StyleSelector styles={stylesExample} />
+          </div>
+          <div className="five">
+            <AddToCart />
+          </div>
+        </div>
+        <ProductDescription />
+      </div>
+    )
+  }
 }
 
 export default ProductOverview;
