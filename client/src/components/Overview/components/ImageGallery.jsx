0cf6f159c85161c56bc6b7a71e0fb50fddfa6695
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-04-01 17:46:23 -0400

    change the size of product's photo on page

--- client/src/components/Overview/components/ImageGallery.jsx
+++ client/src/components/Overview/components/ImageGallery.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageGallery.css';
 
 const ImageGallery = (props)=> {
-  console.log(props.styles.results[2].photos[1].url);
+  // console.log(props.styles.results[2].photos[1].url);
   return (
   <div>
     <img className='current-style' src={props.styles.results[0].photos[0].url}/>
