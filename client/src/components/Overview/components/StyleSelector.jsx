fc82424bcf2a1e2d7400eba227ed8e0d7757c2ed
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-04-01 17:44:07 -0400

    Add styles photos to make it round

--- client/src/components/Overview/components/StyleSelector.jsx
+++ client/src/components/Overview/components/StyleSelector.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import photo from '../../../../img/sgortz.png'
+import './StyleSelector.css';
 
-const StyleSelector = ()=> (
+const StyleSelector = (props)=> {
+
+  return (
   <div>
-    <strong>STYLE > </strong> SELECTED STYLE
-
+    <strong>STYLE > </strong>
+    <br/>
+    <img className="sgortz" src={photo} />
+    <img className="sgortz" src={photo} />
+    <img className="sgortz" src={photo} />
+    <img className="sgortz" src={photo} />
+    <br/>
+    <img className="sgortz" src={photo} />
+    <img className="sgortz" src={photo} />
+    <img className="sgortz" src={photo} />
+    <img className="sgortz" src={photo} />
   </div>
-)
+)}
 export default StyleSelector;
