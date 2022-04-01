ba69530824ee41d74a7a49cbfbf74173685aaa2f
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-03-31 22:07:51 -0400

    Create component and styling for sale announcement

---
+++ client/src/components/Overview/components/Announcements.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Announcements.css'
+
+const Announcements = () => (
+  <small className="announcements">
+    SITE-WIDE ANNOUNCEMENT MESSAGE - SALE / DISCOUNT
+    <strong> OFFER - </strong>
+    <span>NEW PRODUCT HIGHLIGHT</span>
+  </small>
+)
+
+export default Announcements;
