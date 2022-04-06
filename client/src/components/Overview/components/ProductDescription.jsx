73e4cdc14833fba0c2b4f35219815db35ed849b0
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-04-06 19:27:53 -0400

    Temporarily remove grid -- Social Media icons added

--- client/src/components/Overview/components/ProductDescription.jsx
+++ client/src/components/Overview/components/ProductDescription.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import './ProductDescription.css';
+// import {
+//   FacebookShareButton,
+//   InstapaperShareButton,
+//   TwitterShareButton,
+// } from "react-share";
 
-const ProductDescription = (props) => {
-  // console.log(props.features)
-  return (
-  <div className="about-style">
-    <p className="product-slogan">{props.product.slogan}</p>
-    <p className="product-description">{props.product.description}</p>
-    <hr className="divider" />
-    <p className="clothes-features"></p>
-  </div>
-)}
+import { BsInstagram, BsFacebook, BsTwitter } from "react-icons/bs";
+
+const ProductDescription = ({ features }) => {
+  if (features === undefined) {
+    return null;
+  } else {
+    return (
+      <div className="about-style">
+        <p className="product-slogan">{features.slogan}</p>
+        <p className="product-description">{features.description}</p>
+        <hr className="divider" />
+        {features.features.map((feature, index) => {
+          return (
+            <p className="clothes-features" key={index} >
+              {feature.feature}: {feature.value}
+            </p>
+          )
+        })}
+        <span className="social-media">
+          <a href="#"><BsInstagram /></a>
+          <a href="#"><BsFacebook /></a>
+          <a href="#"><BsTwitter /></a>
+        </span>
+      </div>
+    )
+  }
+}
 
 export default ProductDescription;
