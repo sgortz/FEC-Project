fb37542a93f28cb802e12903ca9f6c3c9d3ac36e
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-04-09 15:40:52 -0400

    Scroll to the top of the page button added

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,6 +6,8 @@ import RatingsAndReviews from './RatingsAndReviews/RatingsReviews.jsx';
 import NavBar from './Overview/components/NavBar.jsx';
 import { useInView } from 'react-intersection-observer';
 import useLocalStorage from "use-local-storage";
+import { Link } from 'react-scroll';
+import { BsArrowBarUp } from 'react-icons/bs'
 
 
 
@@ -19,19 +21,25 @@ const App = (props) => {
   const [reviewLength, setReviewLength] = useState(null);
 
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [theme, setTheme]= useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
 
   return (
 
     <div className="app" data-theme={theme}>
-      <NavBar productName={productName} avgReviewRating={avgReviewRating} reviewLength={reviewLength} questionLength={questionLength} inView={inView} theme={theme} setTheme={setTheme}/>
+      <NavBar productName={productName} avgReviewRating={avgReviewRating} reviewLength={reviewLength} questionLength={questionLength} inView={inView} theme={theme} setTheme={setTheme} />
       <div ref={ref}>
-        <ProductOverview product_id={product_id}/>
+        <ProductOverview product_id={product_id} />
       </div>
-      <RelatedProducts product_id={product_id} setProduct_id={setProduct_id} avgReviewRating={avgReviewRating} setProductName={setProductName}/>
+      <RelatedProducts product_id={product_id} setProduct_id={setProduct_id} avgReviewRating={avgReviewRating} setProductName={setProductName} />
       <QuestionAndAnswers product_id={product_id} setQuestionLength={setQuestionLength} />
-      <RatingsAndReviews product_id={product_id} productName={productName} setAvgReviewRating={setAvgReviewRating} setReviewLength={setReviewLength}/>
+      <RatingsAndReviews product_id={product_id} productName={productName} setAvgReviewRating={setAvgReviewRating} setReviewLength={setReviewLength} />
+      <button className="scroll-top">
+        <Link activeClass="active" to="app" spy={true} smooth={true}>
+          <p>TOP <BsArrowBarUp /> </p>
+        </Link>
+      </button>
+
     </div>
 
   )
