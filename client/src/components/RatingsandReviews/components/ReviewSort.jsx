72d7c060a91612eeeeb23be5bc83073b520cdda4
Katy Feng <97313452+katyfsy@users.noreply.github.com>
2022-04-09 17:08:24 -0400

    sort dropdown styling

--- client/src/components/RatingsandReviews/components/ReviewSort.jsx
+++ client/src/components/RatingsandReviews/components/ReviewSort.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import '../styling/ReviewSort.css';
+import '../styling/ReviewSort.css';
 
 function ReviewSort ({reviews, setSortoption}) {
 
@@ -8,24 +8,13 @@ function ReviewSort ({reviews, setSortoption}) {
     <form className='ReviewSort'>
     <label className='ReviewSortLabel'>
     {reviews.length} reviews, sorted by
-      <select id='reviewsortdropdown' onChange={(e)=>setSortoption(e.target.value)}>
+      <select id="reviewsortdropdown" onChange={(e)=>setSortoption(e.target.value)}>
         <option value="relevant">relevant</option>
         <option value="helpful">helpful</option>
         <option value="newest">newest</option>
       </select>
     </label>
   </form>
-
-
-  // <div className='ReviewSort'>
-	//   	<input class="dropdown" type="checkbox" id="dropdown" name="dropdown"/>
-	//   	<label class="for-dropdown" for="dropdown">Dropdown Menu </label>
-  // 		<div class="section-dropdown">
-  // 			<a href="#RatingsandReviews">Dropdown Link </a>
-  // 			<a href="#RatingsandReviews">Dropdown Link </a>
-  // 			<a href="#RatingsandReviews">Dropdown Link </a>
-  // 		</div>
-  // </div>
   )
 
 };
