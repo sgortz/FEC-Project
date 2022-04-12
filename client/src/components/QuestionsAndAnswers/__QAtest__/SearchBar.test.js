99476b84b1422c6a1d54d395c98fd2df94d7ceca
Huijun Lu <lorwegy@gmail.com>
2022-04-12 16:37:46 -0400

    set up test

---
+++ client/src/components/QuestionsAndAnswers/__QAtest__/SearchBar.test.js
@@ -0,0 +1,10 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import SearchBar from '../components/SearchBar.jsx';
+
+
+it('should render Q&A widget', () => {
+  render(<SearchBar searchTerm = 'hello'/>);
+  const inputElement = screen.getByText(/hello/i);
+  expect(inputElement).toBeInTheDocument();
+})
